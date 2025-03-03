2606cacb404add13c95072e810d84f588b74e4e9
Ali Baghdadi <abgr8gr8@gmail.com>
2025-03-03 05:48:38 +0330

    Step 65

--- script.js
+++ script.js
@@ -134,6 +134,10 @@ const highlightCurrentSong = () => {
     playlistSongElements.forEach((songEl) => {
         songEl.removeAttribute("aria-current");
     });
+
+    if (songToHighlight) {
+        songToHighlight.setAttribute("aria-current", "true");
+    }
 };
 
 const renderSongs = array => {
