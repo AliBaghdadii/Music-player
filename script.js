aa9e6a9a57415f6232325b6280d720cd3118727a
Ali Baghdadi <abgr8gr8@gmail.com>
2025-03-03 05:57:28 +0330

    Step 68

--- script.js
+++ script.js
@@ -130,7 +130,8 @@ const playPreviousSong = () => {
 };
 
 const setPlayerDisplay = () => {
-    
+    const playingSong = document.getElementById("player-song-title");
+    const songArtist = document.getElementById("player-song-artist");
 };
 
 const highlightCurrentSong = () => {
