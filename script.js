55b364d1315a8d1f99586fdb8752beecab0a10cc
Ali Baghdadi <abgr8gr8@gmail.com>
2025-03-03 23:57:56 +0330

    Step 99

--- script.js
+++ script.js
@@ -215,6 +215,10 @@ audio.addEventListener("ended", () => {
     } else {
         userData.currentSong = null;
         userData.songCurrentTime = 0;
+        pauseSong();
+        setPlayerDisplay();
+        highlightCurrentSong();
+        setPlayButtonAccessibleText();
     }
 });
 
